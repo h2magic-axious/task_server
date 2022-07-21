f6911ca7da9a102b576cf45e3a15d1cb259432ed
h2magic <h2magic@126.com>
2022-07-21 21:43:25 +0800

    replace of datetime type

--- db/src/models.rs
+++ db/src/models.rs
@@ -1,5 +1,5 @@
 use serde::{Serialize, Deserialize};
-use chrono::NaiveDateTime;
+use chrono::{DateTime, Utc};
 use sqlx::postgres::PgPool;
 
 #[derive(Serialize, Deserialize, Debug, Clone, sqlx::FromRow)]
@@ -8,8 +8,8 @@ pub struct Task {
     pub title: String,
     pub effective: Option<bool>,
     pub lifetime: Option<i32>,
-    pub created_time: Option<NaiveDateTime>,
-    pub doing_time: NaiveDateTime,
+    pub created_time: Option<DateTime<Utc>>,
+    pub doing_time: DateTime<Utc>,
     pub is_loop: Option<bool>,
     pub running: serde_json::Value,
     pub failed: Option<serde_json::Value>,
@@ -19,6 +19,5 @@ async fn fetch_latest_tasks(pool: &PgPool) -> Result<Vec<Task>, sqlx::Error> {
     let rows = sqlx::query_as!(Task, "SELECT * FROM tasks",)
         .fetch_all(pool)
         .await?;
-
     Ok(rows)
 }
---
+++ migrations/20220721133600_insert1.sql
@@ -0,0 +1,2 @@
+-- Add migration script here
+INSERT INTO tasks (title, created_time, doing_time, running) VALUES ('test1', '2022-07-21 21:36:00', '2022-07-21 21:37:00', '{"hello": "world"}');
