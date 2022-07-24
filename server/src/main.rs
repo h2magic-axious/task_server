c4909fa58eff01053d07fda021497258726438fc
h2magic <h2magic@126.com>
2022-07-24 19:17:37 +0800

    using one_cell to build one_cell pool

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,12 @@
 use std::env;
-
+use once_cell::sync::OnceCell;
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
-use sqlx::{Pool, PgPool, Postgres};
+use sqlx::{Pool, Postgres};
 use sqlx::postgres::PgPoolOptions;
 use warp::Filter;
 use warp::reply::Json;
-use lazy_static::lazy_static;
 use db::models::Task;
-
-lazy_static! {
-    static ref POOL: PgPoolOptions = async {
-        dotenv().ok();
-
-        let database_url = env::var("DATABASE_URL")
-            .expect("未找到环境变量: DATABASE_URL");
-        let pool = PgPoolOptions::new()
-            .max_connections(5)
-            .connect(&database_url).await.unwrap();
-        pool
-    };
-}
 
 #[derive(Deserialize, Serialize)]
 struct TaskBody {
@@ -30,6 +16,8 @@ struct TaskBody {
     running: serde_json::Value,
     failed: Option<serde_json::Value>,
 }
+
+static POOL: OnceCell<Pool<Postgres>> = OnceCell::new();
 
 impl TaskBody {
     fn building_task(&self) -> Task {
@@ -43,15 +31,29 @@ impl TaskBody {
     }
 }
 
+async fn pool_builder() -> Pool<Postgres> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("未找到环境变量: DATABASE_URL");
+
+    PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url).await.unwrap()
+}
+
 async fn insert(task_body: TaskBody) -> Result<Json, warp::Rejection> {
     let task = task_body.building_task();
-    let _ = task.insert(&POOL).await;
+    let _ = task.insert(&POOL.get().unwrap()).await;
     let result = warp::reply::json(&task);
     Ok(result)
 }
 
 #[tokio::main]
 async fn main() {
+    let pool = pool_builder().await;
+    POOL.set(pool).unwrap();
+
     let create_task = warp::post()
         .and(warp::path("task"))
         .and(warp::path("create"))
