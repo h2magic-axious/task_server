37c13cd54429bce6608cf5638b33d25b2b8dfc51
h2magic <h2magic@126.com>
2022-07-24 17:35:54 +0800

    can run server

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,11 +2,28 @@
 extern crate dotenv_codegen;
 // extern crate db;
 
+use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPoolOptions;
+use warp::Filter;
 use db::models::Task;
 
+#[derive(Deserialize, Serialize)]
+struct Employee {
+    name: String,
+    rate: u32,
+}
 
 #[tokio::main]
-async fn main(){
-    println!("Hello, world!");
+async fn main() {
+    let promote = warp::post()
+        .and(warp::path("employees"))
+        .and(warp::path::param::<u32>())
+        // Only accept bodies smaller than 16kb...)
+        .and(warp::body::json())
+        .map(|rate, mut employee: Employee| {
+            employee.rate = rate;
+            warp::reply::json(&employee)
+        });
+
+    warp::serve(promote).run(([127, 0, 0, 1], 3030)).await
 }
